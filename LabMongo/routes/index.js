115ad6b72e1c6ad6f0ba496176c3f4298cbfd6bf
fdumani <fabrizio_d_b@hotmail.com>
2018-10-09 22:20:57 -0600

    consultas de read y create
    se agregaron todas las consultas de read y se hizo un espacio para crear películas y productoras

--- LabMongo/routes/index.js
+++ LabMongo/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var mongodb = require('mongodb');
 
 var busqueda = {};
+var duracionProd = 0;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -28,6 +29,36 @@ router.get('/todasPeli', function(req, res){
         } else if (result.length){
           res.render('todasPeli', {
             todasPeli : result
+          });
+        } else {
+          res.send('No documents found');
+        }
+
+        db.close();
+      });
+    }
+  });
+});
+
+router.get('/todasProd', function(req, res){
+  var MongoClient = mongodb.MongoClient;
+
+  var url = 'mongodb://localhost:27017/LabMongo';
+
+  MongoClient.connect(url, function(err, db){
+    if (err){
+      console.log('Unable to connect to server', err);
+    } else {
+      console.log("Connection established");
+
+      var collection = db.collection('productoras');
+
+      collection.find({}).toArray(function(err, result){
+        if (err){
+          res.send(err);
+        } else if (result.length){
+          res.render('todasProd', {
+            todasProd : result
           });
         } else {
           res.send('No documents found');
@@ -112,7 +143,7 @@ router.post('/mostrarFran', function(req, res){
 });
 
 router.get('/buscarDia', function(req, res){
-  res.render('buscarDia', {title: 'Buscar pelicula por franquicia'});
+  res.render('buscarDia', {title: 'Buscar pelicula por año'});
 });
 
 router.get('/resDia', function(req, res){
@@ -150,11 +181,110 @@ router.post('/mostrarDia', function(req, res){
 });
 
 
+router.get('/buscarProd', function(req, res){
+  res.render('buscarProd', {title: 'Buscar pelicula por producdora'});
+});
 
+router.get('/resProd', function(req, res){
+  res.render('resProd', {peli: busqueda, durc: duracionProd});
+});
 
+router.post('/mostrarProd', function(req, res){
+  var MongoClient = mongodb.MongoClient;
 
+  var url = 'mongodb://localhost:27017/LabMongo';
+  MongoClient.connect(url, function(err, db){
+    if (err){
+      console.log('Unable to connect to server', err);
+    } else {
+      console.log('Connectes to server');
 
-router.post('/addstudent', function(req, res){
+      var collection = db.collection('peliculas');
+
+      var peliculasS = req.body.nombrep;
+
+      collection.find({productor:peliculasS}).toArray(function(err, result){
+        if (err){
+          res.send(err);
+        } else {
+          busqueda = result;
+          var tiempo = 0;
+          var x;
+          for (x in result){
+            tiempo += parseInt(result[x].duracion, 10);
+          }
+          duracionProd = tiempo/(result.length);
+
+          res.redirect("resProd");
+        }
+
+        db.close();
+      });
+    }
+  });
+});
+
+router.get('/dataPeli', function(req, res){
+  var MongoClient = mongodb.MongoClient;
+
+  var url = 'mongodb://localhost:27017/LabMongo';
+
+  MongoClient.connect(url, function(err, db){
+    if (err){
+      console.log('Unable to connect to server', err);
+    } else {
+      console.log("Connection established");
+
+      var collection = db.collection('peliculas');
+
+      var conteoFin = 0;
+      var menorFin = {};
+      var maxFin = {};
+
+      collection.count({}, function(err, conteo){
+        if (err){
+          res.send(err);
+        } else {
+          conteoFin = conteo;
+          collection.find({}).sort({duracion: 1}).limit(1).toArray(function(err, menor){
+
+            if (err){
+              res.send(err);
+            } else {
+    
+                menorFin = menor[0];
+                collection.find({}).sort({duracion: -1}).limit(1).toArray(function(err, maximo){
+
+                  if (err){
+                    res.send(err);
+                  } else {
+          
+                      maxFin = maximo[0];
+                  } 
+                  res.render('dataPeli', {contPeli : conteoFin, menorPeli : menorFin, maxPeli : maxFin});
+                  db.close();
+                  
+                });
+            } 
+            
+          });
+        }
+
+      });
+
+     
+
+      
+      
+    }
+  });
+});
+
+router.get('/newPeli', function(req, res){
+  res.render('newPeli', {title: 'Agregar una nueva película'});
+});
+
+router.post('/addPeli', function(req, res){
   var MongoClient = mongodb.MongoClient;
   
 
@@ -165,15 +295,46 @@ router.post('/addstudent', function(req, res){
     } else {
       console.log('Connectes to server');
 
-      var collection = db.collection('students');
+      var collection = db.collection('peliculas');
 
-      var student1 = {student: req.body.student, street: req.body.street, city: req.body.city, state: req.body.state, sex: req.body.sex, gpa: req.body.gpa};
+      var peli1 = {nombre: req.body.nombre, genero: req.body.genero, director: req.body.director, franquicia: req.body.franquicia, pais: req.body.pais, year: req.body.year, duracion: req.body.duracion, productor: req.body.productos, actores: req.body.actores};
 
-      collection.insert([student1], function(err, result){
+      collection.insert([peli1], function(err, result){
         if(err){
           console.log(err);
         } else{
-          res.redirect("thelist");
+          res.redirect("/");
+        }
+        db.close();
+      });
+    }
+  });
+});
+
+router.get('/newProd', function(req, res){
+  res.render('newProd', {title: 'Agregar una nueva productora'});
+});
+
+router.post('/addProd', function(req, res){
+  var MongoClient = mongodb.MongoClient;
+  
+
+  var url = 'mongodb://localhost:27017/LabMongo';
+  MongoClient.connect(url, function(err, db){
+    if (err){
+      console.log('Unable to connect to server', err);
+    } else {
+      console.log('Connectes to server');
+
+      var collection = db.collection('productoras');
+
+      var prod1 = {nombre: req.body.nombre, year: req.body.year, web: req.body.web};
+
+      collection.insert([prod1], function(err, result){
+        if(err){
+          console.log(err);
+        } else{
+          res.redirect("/");
         }
         db.close();
       });
